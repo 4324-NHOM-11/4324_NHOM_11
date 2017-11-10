e88ee67908a6110f9fda5e1159fc9b10e0cf2ffc
Nguyenthithuthuy <ruaco8575@gmail.com>
2017-11-10 10:12:56 +0700

    CSDL_webTinTucKhanhHoa
    Cơ sở dữ liệu của web tin tức Khánh Hòa

---
+++ web_tin_tuc.sql
@@ -0,0 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2017 at 02:28 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `web_tin_tuc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gioithieu`
+--
+
+CREATE TABLE `gioithieu` (
+  `maGioiThieu` int(11) NOT NULL,
+  `tenGioiThieu` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `mota` text COLLATE utf8_unicode_ci NOT NULL,
+  `hoatdong` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lienhe`
+--
+
+CREATE TABLE `lienhe` (
+  `maLienHe` int(11) NOT NULL,
+  `hoTen` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `Email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `Phone` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `noiDung` text COLLATE utf8_unicode_ci NOT NULL,
+  `thoiGian` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `theloai`
+--
+
+CREATE TABLE `theloai` (
+  `maTheLoai` int(11) NOT NULL,
+  `tenTheLoai` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tintuc`
+--
+
+CREATE TABLE `tintuc` (
+  `maTinTuc` int(11) NOT NULL,
+  `tenTinTuc` text COLLATE utf8_unicode_ci NOT NULL,
+  `moTaND` text COLLATE utf8_unicode_ci NOT NULL,
+  `hinhAnh` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `thoiGian` date NOT NULL,
+  `noiDung` text COLLATE utf8_unicode_ci NOT NULL,
+  `maTheLoai` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `IdUser` int(11) NOT NULL,
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `FullName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Phone` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`IdUser`, `username`, `password`, `FullName`, `Address`, `Phone`) VALUES
+(1, 'admin', '12345', 'Admin', 'Nha Trang Khánh Hòa', '0969854561');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gioithieu`
+--
+ALTER TABLE `gioithieu`
+  ADD PRIMARY KEY (`maGioiThieu`);
+
+--
+-- Indexes for table `lienhe`
+--
+ALTER TABLE `lienhe`
+  ADD PRIMARY KEY (`maLienHe`);
+
+--
+-- Indexes for table `theloai`
+--
+ALTER TABLE `theloai`
+  ADD PRIMARY KEY (`maTheLoai`);
+
+--
+-- Indexes for table `tintuc`
+--
+ALTER TABLE `tintuc`
+  ADD PRIMARY KEY (`maTinTuc`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`IdUser`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gioithieu`
+--
+ALTER TABLE `gioithieu`
+  MODIFY `maGioiThieu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lienhe`
+--
+ALTER TABLE `lienhe`
+  MODIFY `maLienHe` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `theloai`
+--
+ALTER TABLE `theloai`
+  MODIFY `maTheLoai` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tintuc`
+--
+ALTER TABLE `tintuc`
+  MODIFY `maTinTuc` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `IdUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
